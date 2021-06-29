6b2036bd62f51d7995d5866254a0be4633dc3afc
firefirehot <rwyat002@ucr.edu>
2021-06-29 10:17:43 -0700

    implemented jumping and double jumping
    I implemented jumping and double jumping. Right now it is using IRL physics which makes it feel very bad. We will probably need to make our own imitation gravity.

--- Wizard-hunter/Assets/scripts folder/controlrt_script.cs
+++ Wizard-hunter/Assets/scripts folder/controlrt_script.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//credit to unity documentation https://docs.unity3d.com/ScriptReference/Rigidbody2D-velocity.html
+//credit to youtube video by samyam https://www.youtube.com/watch?v=3Ad1wr3qBRw&t=189s   and   https://www.youtube.com/watch?v=1Ll1fy2EehU  and  https://www.youtube.com/watch?v=on9nwbZngyw&t=376s
+//crdit to youtube video by Coding With Unity https://www.youtube.com/watch?v=VHYke1HrlMY
+//credit to youtube video by Brackeys https://www.youtube.com/watch?v=gAB64vfbrhI
+//credit to youtube video by Code Monkey https://www.youtube.com/watch?v=c3iEl5AwUF8&t=659s
+
 public class controlrt_script : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    [SerializeField] private LayerMask platformLayerMask;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
     private float movementSpeed = 2f; // player movement speed, change this to slow/speed the player up. This does not change the animation at all
     private bool facingRight = true; // A boolian value that is true if the player is facing right
     SpriteRenderer m_SpriteRenderer; //empty sprite render container
@@ -13,16 +22,21 @@ public class controlrt_script : MonoBehaviour
     private float dashCatcher; // records time. If the player presses the same direction twice in a short amount of time the player dashes.
     private float lastInput = 1;//records the direction of the last input
     private float dashLock = 0;//locks out any other input while dashing. 
-    private float input;//current input. NOT to be confused with "Input" which is defined in unity.
+    private float inputLR;//current left or right input. NOT to be confused with "Input" which is defined in unity.
+    private float inputUD;//current up or down input. NOT to be confused with "Input" which is defined in unity.
     private float timeRunning = 0f;
+    private bool canAirJump = true;
+    private bool canAirDash = true;
     //private int dashPlayer = 3;
 
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         m_SpriteRenderer = GetComponent<SpriteRenderer>(); // assigns empty container with the actual component
         facingRight = true; 
         ani = GetComponent<Animator>(); // assigns empty container with the actual component
+        boxCollider = GetComponent<BoxCollider2D>();
 
     }
 
@@ -32,44 +46,67 @@ public class controlrt_script : MonoBehaviour
 
     void Update()
     {
+        
         if (dashLock <= 0)
         {
             ani.SetBool("dashing", false);
 
             if (Input.GetKey("d"))
-                input = 1;
+                inputLR = 1;
             else if (Input.GetKey("a"))
-                input = -1;
+                inputLR = -1;
             else
-                input = 0;
-            transform.position = transform.position + new Vector3(input * Time.deltaTime * movementSpeed, 0, 0);//moves the character by getting the characters current position and 
-                                                                                                                //adding "input*Time.deltaTime*movementSpeed"(direction = input, Time.deltaTime = time since last frame, movementSpeed multiplier)
+                inputLR = 0;
 
-            if (input < 0 && facingRight == true || input > 0 && facingRight == false)
+            if (Input.GetKeyDown("w"))
+                inputUD = 1;
+            else if (Input.GetKey("s"))
+                inputUD = -1;
+            else
+                inputUD = 0;
+
+            transform.position = transform.position + new Vector3(inputLR * Time.deltaTime * movementSpeed, 0, 0);//moves the character by getting the characters current position and 
+                                                                                                                  //adding "input*Time.deltaTime*movementSpeed"(direction = inputLR, Time.deltaTime = time since last frame, movementSpeed multiplier)
+
+
+            if (isGrounded()) {
+                canAirJump = true;
+                canAirDash = true;
+            }
+            if (inputUD == 1 && isGrounded())//enables jump if player is on the ground and they press up
+                rb.velocity = new Vector2(0f, 5f);//this determines the speed at which pressing up will propell you at
+            else if(inputUD == 1 && canAirJump) {//enables jump if player has not double jumped yet and they press up
+                rb.velocity = new Vector2(0f, 5f);//this determines the speed at which pressing up will propell you at
+                canAirJump = false;
+            }
+
+            if (inputLR < 0 && facingRight == true || inputLR > 0 && facingRight == false)
             {//flips the sprite if we get a input in the opposite direction that the sprite is facing
                 m_SpriteRenderer.flipX = !m_SpriteRenderer.flipX;//flips the sprite
                 facingRight = !facingRight;//records the direction sprite is facing
 
             }
-            if (input == 0)
+            if (inputLR == 0)
             {//changes animation to idle if no input is detected
                 ani.SetBool("isRunning", false);
                 dashCatcher = dashCatcher + Time.deltaTime;//keeps track of the how much time it has been since no input has been detected
                 resetMovementSpeed();//resets movement speed
 
             }
-            else if (input != 0)
+            else if (inputLR != 0)
             {// changes animation to running if an input is detected
 
-                if ((dashCatcher < 0.3 && dashCatcher > 0.0001 && ((lastInput > 0 && input > 0) || (lastInput < 0 && input < 0))))
+                if ((dashCatcher < 0.3 && dashCatcher > 0.0001 && ((lastInput > 0 && inputLR > 0) || (lastInput < 0 && inputLR < 0))) && canAirDash)
                 {
                     ani.SetBool("isRunning", false);
                     ani.SetBool("dashing",true);
                     dashLock = 0.2F;//sets the dashLock to prevent user input for 0.2 seconds
+                    if (!isGrounded())
+                        canAirDash = false;
                 }
                 else
                     ani.SetBool("isRunning", true);
-                lastInput = input;
+                lastInput = inputLR;
                 dashCatcher = 0F;
                 increaseMovementSpeed();//increases movement speed to a max of 5f to immitate momentum
             }
@@ -88,6 +125,11 @@ public class controlrt_script : MonoBehaviour
 
 
 
+    }
+
+    bool isGrounded() {//credit to Code Monkey
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 2f, platformLayerMask);// casts ray with (center of player, player collider size, rotation, direction, added size to detect, layers to hit)
+        return raycastHit.collider != null;//returns true if it collided, false if it didn't
     }
 
 
