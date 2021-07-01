247ad6ad27002aace6e8dfd4118233bc36dca84c
firefirehot <rwyat002@ucr.edu>
2021-06-30 20:50:26 -0700

    Implemented wall jumping
    Since we did not have an animation for wall jumping I had to make my own. It isn't great but I think it will do.  Currently the wall jump enables the player to get more and more height because gravity is so low. Once gravity is implemented we can adjust the wall jump to prevent the player from climbing the wall all the way to the top.

--- Wizard-hunter/Assets/scripts folder/controlrt_script.cs
+++ Wizard-hunter/Assets/scripts folder/controlrt_script.cs
@@ -13,9 +13,10 @@ public class controlrt_script : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private LayerMask wallLayerMask;
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
-    private float movementSpeed = 2f; // player movement speed, change this to slow/speed the player up. This does not change the animation at all
+  
     private bool facingRight = true; // A boolian value that is true if the player is facing right
     SpriteRenderer m_SpriteRenderer; //empty sprite render container
     private Animator ani;// empty animator container, animator decides which animation to play
@@ -24,10 +25,13 @@ public class controlrt_script : MonoBehaviour
     private float dashLock = 0;//locks out any other input while dashing. 
     private float inputLR;//current left or right input. NOT to be confused with "Input" which is defined in unity.
     private float inputUD;//current up or down input. NOT to be confused with "Input" which is defined in unity.
-    private float timeRunning = 0f;
-    private bool canAirJump = true;
-    private bool canAirDash = true;
-    private bool prepDoubleJump = false;
+    
+
+    private bool canAirJump = true;//set to true when u want to enable a second jump along with prepDoubleJump
+    private bool prepDoubleJump = false; //set to true when u want to enable a second jump along with canAirJump
+
+    private bool canAirDash = true; // set to true when u want to enable a dash
+
     private float saveVelocity = 5f;
     //private int dashPlayer = 3;
 
@@ -48,7 +52,7 @@ public class controlrt_script : MonoBehaviour
 
     void Update()
     {
-        
+
         if (dashLock <= 0)
         {
             ani.SetBool("dashing", false);
@@ -67,51 +71,49 @@ public class controlrt_script : MonoBehaviour
             else
                 inputUD = 0;
 
-            //transform.position = transform.position + new Vector3(inputLR * Time.deltaTime * movementSpeed, 0, 0);//moves the character by getting the characters current position and 
-            //adding "input*Time.deltaTime*movementSpeed"(direction = inputLR, Time.deltaTime = time since last frame, movementSpeed multiplier)
 
-            //rb.velocity = new Vector2(inputLR * Time.deltaTime * movementSpeed,0);
             if (rb.velocity.x < 5f && rb.velocity.x > -5f)
                 rb.velocity = rb.velocity + new Vector2(inputLR * Time.deltaTime * 10, 0);
-            if (isGrounded()) {
+
+            if (isGrounded())
+            {
                 canAirJump = true;
                 canAirDash = true;
             }
+
+
             if (inputUD == 1 && isGrounded() && prepDoubleJump == false)
             {//enables jump if player is on the ground and they press up
-                Debug.Log("active");
                 rb.velocity = new Vector2(rb.velocity.x, 5f);//this determines the speed at which pressing up will propell you at
-                prepDoubleJump = true;
+                prepDoubleJump = true;//enables the double jump after the first jump
             }
             else if (inputUD == 1 && canAirJump && prepDoubleJump == true)
             {//enables jump if player has not double jumped yet and they press up
-                Debug.Log("print1");
                 rb.velocity = new Vector2(rb.velocity.x, 5f);//this determines the speed at which pressing up will propell you at
-                canAirJump = false;
-                prepDoubleJump = false;
+                canAirJump = false;//disables any jumps in air
+                prepDoubleJump = false;//disables the double jump
             }
 
             if (inputLR < 0 && facingRight == true || inputLR > 0 && facingRight == false)
             {//flips the sprite if we get a input in the opposite direction that the sprite is facing
                 m_SpriteRenderer.flipX = !m_SpriteRenderer.flipX;//flips the sprite
                 facingRight = !facingRight;//records the direction sprite is facing
-                
-
             }
-            if (inputLR == 0)
+
+
+            if (inputLR == 0)//if we don't detect a LR(left or right) input we enter this loop and play the idle animation
             {//changes animation to idle if no input is detected
                 ani.SetBool("isRunning", false);
                 dashCatcher = dashCatcher + Time.deltaTime;//keeps track of the how much time it has been since no input has been detected
-                if (isGrounded()) {
-                    rb.velocity = new Vector2(rb.velocity.x + (-rb.velocity.x * Time.deltaTime*4f), rb.velocity.y);
+                if (isGrounded())//if we are on the floor we want to come to a stop since we no input is being given
+                {
+                    rb.velocity = new Vector2(rb.velocity.x + (-rb.velocity.x * Time.deltaTime * 4f), rb.velocity.y);
                 }
-                resetMovementSpeed();//resets movement speed
 
             }
             else if (inputLR != 0)
             {// changes animation to running if an input is detected
-
-                if ((dashCatcher < 0.3 && dashCatcher > 0.0001 && ((lastInput > 0 && inputLR > 0) || (lastInput < 0 && inputLR < 0))) && canAirDash)
+                if ((dashCatcher < 0.3 && dashCatcher > 0.0001 && ((lastInput > 0 && inputLR > 0) || (lastInput < 0 && inputLR < 0))) && canAirDash)//if a second input LR(left or right) is detected within 0.001s->0.3s then we dash
                 {
                     ani.SetBool("isRunning", false);
                     ani.SetBool("dashing", true);
@@ -119,61 +121,83 @@ public class controlrt_script : MonoBehaviour
                     if (!isGrounded())
                         canAirDash = false;
                 }
-                else
+                else//if we don't dash we are running at this point
                 {
                     ani.SetBool("isRunning", true);
                     saveVelocity = rb.velocity.x;
                 }
                 lastInput = inputLR;
                 dashCatcher = 0F;
-                increaseMovementSpeed();//increases movement speed to a max of 5f to immitate momentum
+                
             }
+
+
+            if (isAgainstWall() && !isGrounded() && inputUD == 1)//activates if player is against a wall and is not on the floor and the player pressed up. This will overwrite any velocity the player had before.
+            {
+                
+                inputUD = 0;
+                if (lastInput > 0)
+                {
+                    rb.velocity = new Vector2(-4f, 3f);//gives player a new velocity of -4x and 3y after wall jumping
+                    ani.SetTrigger("wallJump"); //wall jump animation plays
+
+                }
+                if (lastInput < 0)
+                {
+                    rb.velocity = new Vector2(4f, 3f); //gives player a new velocity of 4x and 3y after wall jumping
+                    ani.SetTrigger("wallJump_unflipped");//flipped version of the wall jump animation plays
+                }
+                facingRight = !facingRight;//records the direction sprite is facing
+                m_SpriteRenderer.flipX = !m_SpriteRenderer.flipX;//flips the sprite
+                canAirDash = true;//enable air dash
+                canAirJump = true;//enable double jump (part1)
+                prepDoubleJump = true;//enable double jump(part2)
+                
+                
+            }
+
+
         }
-        else {
+        else if (dashLock > 0)
+        {
             if (lastInput > 0)
-                transform.position = transform.position + new Vector3(Time.deltaTime*20F, 0, 0);//
+                transform.position = transform.position + new Vector3(Time.deltaTime * 20F, 0, 0);//this is the dash movement if looking right
             else
-                transform.position = transform.position + new Vector3(-Time.deltaTime * 20F, 0, 0);
-            
+                transform.position = transform.position + new Vector3(-Time.deltaTime * 20F, 0, 0);//this is the dash movement if looking left
+
             dashLock = dashLock - Time.deltaTime;//decrements the dashlock until 0.2 seconds have passed
-            if (dashLock <= 0)
+            if (dashLock <= 0)//when the dashlock is over this if statment gives the player back their velocity that they had before the dash
             {
                 if (saveVelocity < -3f && saveVelocity > 3f)
                     rb.velocity = new Vector2(saveVelocity, 0);
-                else {
-                    rb.velocity = new Vector2(lastInput * 3,0);
+                else
+                {
+                    rb.velocity = new Vector2(lastInput * 3, 0);
                 }
             }
-        }
+        }//end of dashlock
 
 
 
 
 
 
-    }
+    }//end of update
 
     bool isGrounded() {//credit to Code Monkey
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, platformLayerMask);// casts ray with (center of player, player collider size, rotation, direction, added size to detect, layers to hit)
         return raycastHit.collider != null;//returns true if it collided, false if it didn't
     }
 
+    bool isAgainstWall() {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size*1.1f, 0f, Vector2.right, 0f, wallLayerMask);// casts ray with (center of player, player collider size, rotation, direction, added size to detect, layers to hit)
+        
+        //return (raycastHit.collider != null || raycastHit2 != null);//returns true if it collided, false if it didn't
+        return raycastHit.collider != null;
+    }
 
-    void increaseMovementSpeed()
-    {
-        if (movementSpeed < 5f) {
-            movementSpeed = movementSpeed + ((Time.deltaTime*timeRunning)*5 / 3f);//increases movementspeed over ~3 seconds (acomplished using testing, not math)
-            timeRunning = timeRunning + Time.deltaTime;
-        }
-        else
-        {
-            movementSpeed = 5f;//max movementspeed will be 5
-        }
-    }
-    void resetMovementSpeed() {// reset mmovement speed so that player will need to rebuild it.
-        movementSpeed = 2f;
-        timeRunning = 0;
-    }
+
+
 
 
 }
