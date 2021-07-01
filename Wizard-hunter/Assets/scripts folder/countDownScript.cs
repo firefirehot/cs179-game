e5e434e83cef9b42af29b1f8352db4c2a17b0549
firefirehot <rwyat002@ucr.edu>
2021-06-30 22:12:58 -0700

    Did the basics of health and timer.
    The health and the timer will always be in the same position relative to the main camera. We will need to move the main camera later to follow the player. The timer counts down as it should but the health need it's own scripts.

---
+++ Wizard-hunter/Assets/scripts folder/countDownScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class countDownScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float initialTime = 300f;
+    private Text myText;
+    //public Text myText;
+    private float TimePassed;
+    void Start()
+    {
+        myText = GetComponent<Text>();
+        TimePassed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeCalc = 0;
+        int minuete = 0;
+        int tenSecond = 0;
+        int oneSecond = 0;
+
+        if (TimePassed < 301f)
+        {
+            TimePassed = Time.deltaTime + TimePassed;
+            timeCalc = initialTime - TimePassed;
+
+            while (timeCalc - 60 >= 0)
+            {
+                timeCalc = timeCalc - 60;
+                minuete++;
+            }
+            while (timeCalc - 10 >= 0)
+            {
+                timeCalc = timeCalc - 10;
+                tenSecond++;
+            }
+            while (timeCalc - 1 >= 0)
+            {
+                timeCalc = timeCalc - 1;
+                oneSecond++;
+            }
+
+            myText.text = minuete.ToString() + ":" + tenSecond.ToString() + oneSecond.ToString();
+        }
+        else {
+            myText.text = "0" + ":" + "0" + "0";
+        }
+
+    }
+}
