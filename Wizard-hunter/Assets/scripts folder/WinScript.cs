d4a476846369b8648b551912941c84ca8c5d9884
firefirehot <rwyat002@ucr.edu>
2021-08-10 13:15:13 -0700

    added man manue button to death and win screen

---
+++ Wizard-hunter/Assets/scripts folder/WinScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void restartGame()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("yes");
+    }
+
+
+}
