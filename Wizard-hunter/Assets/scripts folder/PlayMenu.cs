03f0928bb446e901ceffe1b26d8e9d162a92b110
Dylan McDowell <dmcdo006@ucr.edu>
2021-08-10 10:29:14 -0700

    Update PlayMenu.cs
    - Fixed Scene Load error

--- Wizard-hunter/Assets/scripts folder/PlayMenu.cs
+++ Wizard-hunter/Assets/scripts folder/PlayMenu.cs
@@ -20,7 +20,7 @@ public class PlayMenu : MonoBehaviour
     //PlayGame2_1 will load level 2 wizard 1, when those scenes have been initialized
     public void PlayGame2_1()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(2);
     }
     
     //PlayGame2_2 will load level 2 wizard 2, when those scenes have been initialized
