2563c8740363d9ab9371b25b754495b06936ba69
Dylan McDowell <dmcdo006@ucr.edu>
2021-08-19 13:11:57 -0700

    -Updated Level1_1
    - Music now pauses with pause menu correctly

--- Wizard-hunter/Assets/scripts folder/MusicHandler.cs
+++ Wizard-hunter/Assets/scripts folder/MusicHandler.cs
@@ -9,24 +9,29 @@ public class MusicHandler : MonoBehaviour
     private bool isPaused = false;
     public static GameObject first = null; 
     public static int scene; 
+    [SerializeField] GameObject player;
 
-    //Called to ensure gameObject, if the first, is saved as such and will be preserved on reset
+    //Called to ensure the music source, if the first, is saved as such and will be preserved on reset.
     void Awake(){
         if(first != null){
-            Destroy(gameObject);
+            Destroy(player);
+            aSource = first.GetComponent<AudioSource>();
         }
         else{
-            first = gameObject;
+            first = player;
             scene = SceneManager.GetActiveScene().buildIndex;
-            aSource = GetComponent<AudioSource>();
-            DontDestroyOnLoad(gameObject);
+            aSource = first.GetComponent<AudioSource>();
+            DontDestroyOnLoad(first);
         }
     }
 
     //Update is called every frame. Checks to see if we are in a different scene than what we started in.
     void Update(){
         if(SceneManager.GetActiveScene().buildIndex != scene){
-            Destroy(gameObject);
+            Destroy(first);
+        }
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Pause();
         }
     }
 
