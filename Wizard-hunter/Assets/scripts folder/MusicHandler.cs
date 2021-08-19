9915ebad3dc3e8216ad5470fc631ac763cd5ea3e
Dylan McDowell <dmcdo006@ucr.edu>
2021-08-18 19:19:06 -0700

    -Updated Level1_1
    -Changed Implementation of stopping music to something not inane
    - Added Comments for clarity

--- Wizard-hunter/Assets/scripts folder/MusicHandler.cs
+++ Wizard-hunter/Assets/scripts folder/MusicHandler.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicHandler : MonoBehaviour
 {
     private AudioSource aSource;
     private bool isPaused = false;
     public static GameObject first = null; 
-    [SerializeField] private GameObject audioS;
+    public static int scene; 
 
+    //Called to ensure gameObject, if the first, is saved as such and will be preserved on reset
     void Awake(){
         if(first != null){
-            Destroy(audioS);
+            Destroy(gameObject);
         }
         else{
-            first = audioS;
-            aSource = first.GetComponent<AudioSource>();
-            DontDestroyOnLoad(first);
+            first = gameObject;
+            scene = SceneManager.GetActiveScene().buildIndex;
+            aSource = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    //Update is called every frame. Checks to see if we are in a different scene than what we started in.
+    void Update(){
+        if(SceneManager.GetActiveScene().buildIndex != scene){
+            Destroy(gameObject);
         }
     }
 
@@ -29,12 +39,6 @@ public class MusicHandler : MonoBehaviour
         else{
             aSource.Play(0);
             isPaused = false;
-        }
-    }
-
-    public void QuitTrigger(){
-        if(first != null){
-            Destroy(first);
         }
     }
 }
